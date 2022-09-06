51bf3ab218c4768f11cc5fc8947867c1a4304951
acidmux <112217849+acidmux@users.noreply.github.com>
2022-09-06 06:30:11 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const authRouter = require('./routers/auth.router');
 const adminRouter = require('./routers/admin.router');
 
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://root:9qawwvky4T0bPqhE@cluster0.3gu8lkk.mongodb.net/?retryWrites=true&w=majority';
+
 const app = express();
 
 app.use(express.json());
@@ -12,13 +16,13 @@ app.use(authRouter);
 app.use(adminRouter);
 
 mongoose
-    .connect('mongodb+srv://root:9qawwvky4T0bPqhE@cluster0.3gu8lkk.mongodb.net/?retryWrites=true&w=majority')
+    .connect(DB_URI)
     .then(() => {
         console.log('Database connected successfully');
     }).catch((e) => {
         console.log(e);
     });
 
-app.listen(8080, '0.0.0.0', () => {
-    console.log('Connected at port 3000');
+app.listen(PORT, HOST, () => {
+    console.log(`Started at port ${PORT}`);
 });
